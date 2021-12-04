f42bdfb9c44f540f19e22e74c934b94d0fa93262
zpzgzerg <zpzgzerg@gmail.com>
2021-12-04 16:22:20 +0900

    first commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const db = require('./db');
+
+const app = express();
+
+// app.use(express.json());
+app.use(bodyParser.json());
+
+db.pool.query(`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+    )`, (err, results, fields) => {
+        console.log('results', results);
+    })
+
+app.listen(5000, () => {
+    console.log("애플리케이션이 5000번 포트에서 시작됐습니다.");
+})
+
+app.get("/api/values", function(req, res) {
+    db.pool.query(`SELECT * FROM lists;`, (err, results, fields) => {
+        if(err)
+            return res.status(500).send(err);
+        else
+            return res.json(results);
+    })
+})
+
+app.post("/api/value", function(req, res, next) {
+    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`, (err, results, fields) => {
+        if(err)
+            return res.status(500).send(err);
+        else
+            return res.json({success: true, value: req.body.value});
+    });
+})
